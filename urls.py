41a1156e34d750da173dd55e2484cc4aeac34336
Tyler Backman <tbackman@ucr.edu>
2011-03-02 13:49:03 -0800

    removed test comments

--- urls.py
+++ urls.py
@@ -14,7 +14,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
-# comment added from command line
-# test comment
-# another test comment
-# comment added from web page
