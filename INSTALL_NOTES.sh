054c20caa5c43b54a233b510238b6ff54da0b062
TylerBackman <casioqv@usermail.com>
2013-10-01 16:04:42 -0700

    updated openbabel

--- INSTALL_NOTES.sh
+++ INSTALL_NOTES.sh
@@ -28,7 +28,7 @@ apt-get install -y libapache2-mod-wsgi
 apt-get install -y memcached
 
 # install these w/ sid allowed via apt pinning
-apt-get install -y -t=sid libopenbabel4 openbabel
+# apt-get install -y -t=sid libopenbabel4 openbabel
 
 pip install django-bootstrap-toolkit
 pip install django-cms south
