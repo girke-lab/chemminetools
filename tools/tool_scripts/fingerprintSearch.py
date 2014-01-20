8acf8f2f6a27f5a5ad8f67998f16ec745855aa35
TylerBackman <casioqv@usermail.com>
2014-01-20 09:39:41 -0800

    major updates to fp search tool

--- tools/tool_scripts/fingerprintSearch.py
+++ tools/tool_scripts/fingerprintSearch.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # make it possible to run as standalone program
-
 import sys
 sys.path.append('/srv/chemminetools')
 sys.path.append('/srv/chemminetools/sdftools')  # allow tools.py to be imported
@@ -13,16 +12,36 @@ setup_environ(chemminetools.settings)
 from django.contrib.auth.models import User
 from django.conf import settings
 
+# load functions used
+from ChemmineR.SimilaritySearch import SimilaritySearch
+from sdftools.moleculeformats import sdf_to_smiles
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description='perform pubchem fingerprint search')
+parser.add_argument('-o', '--outfile', help='output file',required=True)
+parser.add_argument('-s', '--similarity', help='similarity cutoff',required=True)
+parser.add_argument('-c', '--compounds', help='max compounds returned',required=True)
+args = vars(parser.parse_args())
+
 def main():
     sdf = sys.stdin.read()
-    user = User.objects.get(id=args['user'])
+    similarity = args['similarity']
+    similarity = int(float(similarity) * 100)
+    compounds = int(args['compounds'])
+    query = sdf_to_smiles(sdf)
 
-    output = addMyCompounds(sdf, user)
+    try:
+        id_list = SimilaritySearch(query, similarity, compounds)
+        if id_list == 'error':
+            raise Exception
+        id_list = [str(cid) for cid in id_list]
+        output = '\n'.join(id_list)
+    except:
+        output = ''
 
     f = open(args['outfile'], 'w')
     f.write(output)
     f.close()
-
 
 if __name__ == '__main__':
     main()
