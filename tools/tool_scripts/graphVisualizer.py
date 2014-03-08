cdcd3cf3853dac83c467e27c12f816ae5725fcaa
TylerBackman <tbackman@ucr.edu>
2014-03-07 17:59:28 -0800

    minor changes in graph gen

--- tools/tool_scripts/graphVisualizer.py
+++ tools/tool_scripts/graphVisualizer.py
@@ -19,8 +19,17 @@ def main():
     fa = sys.stdin.read()
     fa = fa.rstrip()
 
-    outputData = {u'maxZoom': 2, u'minZoom': 0.5, u'elements': {u'nodes': [{u'data': {u'height': 174, u'id': u'j', u'weight': 65, u'name': u'Jerry'}}, {u'data': {u'height': 160, u'id': u'e', u'weight': 48, u'name': u'Elaine'}}, {u'data': {u'height': 185, u'id': u'k', u'weight': 75, u'name': u'Kramer'}}, {u'data': {u'height': 150, u'id': u'g', u'weight': 70, u'name': u'George'}}], u'edges': [{u'data': {u'source': u'j', u'target': u'e'}}, {u'data': {u'source': u'j', u'target': u'k'}}, {u'data': {u'source': u'j', u'target': u'g'}}, {u'data': {u'source': u'e', u'target': u'j'}}, {u'data': {u'source': u'e', u'target': u'k'}}, {u'data': {u'source': u'k', u'target': u'j'}}, {u'data': {u'source': u'k', u'target': u'e'}}, {u'data': {u'source': u'k', u'target': u'g'}}, {u'data': {u'source': u'g', u'target': u'j'}}]}, u'style': {u'1': {u'properties': [{u'name': u'text-outline-color', u'value': u'#000'}, {u'name': u'background-color', u'value': u'#000'}, {u'name': u'target-arrow-color', u'value': u'#000'}, {u'name': u'line-color', u'value': u'#000'}], u'selector': u':selected'}, u'0': {u'properties': [{u'name': u'text-valign', u'value': u'center'}, {u'name': u'color', u'value': u'#fff'}, {u'name': u'content', u'value': u'data(name)'}, {u'name': u'text-outline-color', u'value': u'#888'}, {u'name': u'text-outline-width', u'value': 3}, {u'name': u'font-family', u'value': u'helvetica'}, {u'name': u'font-size', u'value': 14}, {u'name': u'border-color', u'value': u'#fff'}, {u'name': u'height', u'value': u'mapData(height, 0, 200, 10, 45)'}, {u'name': u'width', u'value': u'mapData(weight, 30, 80, 20, 50)'}], u'selector': u'node'}, u'length': 3, u'2': {u'properties': [{u'name': u'width', u'value': 2}, {u'name': u'target-arrow-shape', u'value': u'triangle'}], u'selector': u'edge'}}, u'showOverlay': False}
+    outputData = {
+            u'maxZoom': 2, 
+            u'minZoom': 0.5, 
+            u'style': {
+                u'1': {u'properties': [{u'name': u'text-outline-color', u'value': u'#000'}, {u'name': u'background-color', u'value': u'#000'}, {u'name': u'target-arrow-color', u'value': u'#000'}, {u'name': u'line-color', u'value': u'#000'}], u'selector': u':selected'}, u'0': {u'properties': [{u'name': u'text-valign', u'value': u'center'}, {u'name': u'color', u'value': u'#fff'}, {u'name': u'content', u'value': u'data(name)'}, {u'name': u'text-outline-color', u'value': u'#888'}, {u'name': u'text-outline-width', u'value': 3}, {u'name': u'font-family', u'value': u'helvetica'}, {u'name': u'font-size', u'value': 14}, {u'name': u'border-color', u'value': u'#fff'}, {u'name': u'height', u'value': u'mapData(height, 0, 200, 10, 45)'}, {u'name': u'width', u'value': u'mapData(weight, 30, 80, 20, 50)'}], u'selector': u'node'}, u'length': 3, u'2': {u'properties': [{u'name': u'width', u'value': 2}, {u'name': u'target-arrow-shape', u'value': u'triangle'}], u'selector': u'edge'}}, 
+            u'showOverlay': False}
+
     outputData['elements'] = json.loads(fa)
+    outputData['layout'] = {
+        u'name': u'arbor'
+    }
 
     # write options to output file
     of = open(args['outfile'], 'w')
