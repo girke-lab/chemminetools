cef161e157439e735ebbd7a53e1b86951f2fc104
Kevin Horan <khoran@cs.ucr.edu>
2014-04-29 18:01:55 +0000

    starting on smarts search tool. not done yet

---
+++ tools/tool_scripts/smartsSearch.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+# use: ./smartsSearc.R --outfile=output.txt --pattern=<SMARTS pattern> --distinct < input.sdf
+
+library(ChemmineR)
+library(R.utils)
+
+
+if(!exists("debug_mode")){
+	args = commandArgs(asValues=TRUE)
+    if(is.null(args$outfile))
+        stop("no outfile given")
+    if(is.null(args$pattern))
+        stop("no pattern option given")
+
+	outfile    = args$outfile
+    distinct = ! is.null(args$distinct)
+    pattern = args$pattern
+	
+	# read in sdf from standard i/o
+	f <- file("stdin")
+	open(f)
+	sdfInput <- read.SDFset(read.SDFstr(f))
+	close(f)
+
+}
+
+sdfInput <- sdfInput[validSDF(sdfInput)]
+
+
+results = smartsSearchOB(sdfInput,pattern,uniqueMatches=distinct)
+output = data.frame(cid=sdfid(sdfInput),numMatches = results)
+write.table(output,file=outfile,quote=FALSE,sep=",",row.names=FALSE,col.names=TRUE)
