d6e1629049306094b3b9c795e3ec7d916aac7c82
Vagrant User <vagrant@debian-70rc1-x64-vbox4210.vagrantup.com>
2016-07-15 14:42:12 +0000

    added inchi calc

---
+++ tools/tool_scripts/inchi.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import sys
+import tempfile
+import re
+import pybel
+
+parser = \
+    argparse.ArgumentParser(description='Compute InChI string and return table'
+                            )
+parser.add_argument('-o', '--outfile', help='output file',
+                    required=True)
+args = vars(parser.parse_args())
+
+properties = ['abonds', 'atoms', 'bonds', 'cansmi', 'cansmiNS', 'dbonds', 'formula', 'HBA1', 'HBA2', 'HBD', 'InChI', 'InChIKey', 'L5', 'logP', 'MP', 'MR', 'MW', 'nF', 's', 'sbonds', 'smarts', 'tbonds', 'title', 'TPSA']
+
+def main():
+    fa = sys.stdin.read()
+    fa = fa.rstrip()
+    of = open(args['outfile'], 'w')
+    of.write('cid,')
+    labels = ''
+    for i in properties:
+        labels = labels + i + ','
+    labels = re.match(r"^(.*),", labels).group(1)
+    of.write(labels + '\n')
+    inputTemp = tempfile.NamedTemporaryFile(suffix='.sdf', delete=False)
+    inputTemp.write(fa)
+    inputTempName = inputTemp.name
+    inputTemp.close()
+    for mol in pybel.readfile('sdf', inputTempName):
+        info = ''
+        myid = mol.title
+        mol.addh()
+        mol.make3D()
+        desc = mol.calcdesc()
+        for thisdesc in properties:
+            info = info + str(desc[thisdesc]) + ','
+        info = re.match(r"^(.*),", info).group(1)
+        of.write(myid.strip() + ',' + info + '\n')
+    of.close()
+    os.unlink(inputTempName)
+
+
+if __name__ == '__main__':
+    main()
