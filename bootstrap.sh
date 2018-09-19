9fe1b0d9045d306dfdf1ef11d2cb775ff7f6a32b
root <root@ip-172-31-37-64.us-east-2.compute.internal>
2018-09-19 17:54:12 +0000

    set python version in use explicitly

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,11 @@ set -o xtrace
 
 # for debian 9, both python 2.7 and python 3.5 are installed by default. 
 # this will make python3 the default for cli 
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 update-alternatives --install /usr/bin/python python /usr/bin/python3.5 2
-update-alternatives --install /usr/bin/python python /usr/bin/python3.5 2
+
+#use python 2.7 for install debian packages
+update-alternatives --set python /usr/bin/python2.7
 
 # Install Required Debian Packages (including django) 
 apt-get update
@@ -39,6 +42,8 @@ apt-get install -y libgc1c2 # for fmcs
 # clean up package install 
 apt-get clean
 
+#swithc back to python 3 for the rest
+update-alternatives --set python /usr/bin/python3.5
 
 # For correct dependency resolution pip needs everything on a single line
 pip3 install Django==1.11.14 django-guardian==1.4.9  django-bootstrap-toolkit==2.15.0 \
@@ -46,7 +51,7 @@ django-cms==3.5.2 South==1.0.2  django-appmedia==1.0.1 django-celery==3.2.2 simp
 ghostscript PyYAML==3.12 beautifulsoup4==4.6.1 celery==3.1.26.post2 django-userena==2.0.1 \
 'html5lib<0.99999999' subprocess32 openbabel django-mptt==0.9.1 djangocms_text_ckeditor==3.6.0 \
 djangocms_picture djangocms_link djangocms_file djangocms_googlemap django_cron==0.5.1 \
-python-memcached future
+python-memcached future configparser
 
 # create symbolic link for /srv/chemminetools
 ln -s /vagrant /srv/chemminetools
