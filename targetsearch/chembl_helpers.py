f9412277990e04379ddedbe4787e905708a91a47
khoran <khoran@cs.ucr.edu>
2019-05-16 20:11:12 +0000

    modified  chembl_helpers functions

--- targetsearch/chembl_helpers.py
+++ targetsearch/chembl_helpers.py
@@ -1,11 +1,22 @@
 import psycopg2
 
+
 def tupleArray2Dict(tuples):
     dict = {}
     for tuple in tuples:
         a = dict.get(tuple[0],[])
         a.append(tuple[1])
         dict[tuple[0]] = a
+    return dict
+
+def groupBy(keyFn, tuples):
+    dict = {}
+    for tuple in tuples:
+        key = keyFn(tuple)
+        a = dict.get(key,[])
+        a.append(tuple)
+        dict[key] = a
+
     return dict
 
 def runQuery(query,values):
@@ -17,8 +28,13 @@ def runQuery(query,values):
 
 def chemblTargetAccessions(chemblIds):
     data = runQuery("""
-            select distinct chembl_id_lookup.chembl_id, accession
+            select distinct chembl_id_lookup.chembl_id, accession,
+                    activities.molregno, pref_name, activity_id, assays.chembl_id AS chembl_assay_id, 
+                    component_sequences.description, organism, 
+                    activities.standard_value, activities.standard_units, 
+                    activities.standard_flag, activities.standard_type
                 from chembl_id_lookup
+                        join molecule_dictionary using(molregno)
                         join activities on(entity_id = molregno)
                         join assays using(assay_id)
                         join target_components using(tid)
@@ -27,7 +43,26 @@ def chemblTargetAccessions(chemblIds):
                         chembl_id_lookup.chembl_id in %s
                 order by 1
             """,(chemblIds,))
-    return tupleArray2Dict(data)
+    #return tupleArray2Dict(data)
+    return groupBy(lambda t: t[0], data)
+def chemblTargetAccessionsByAnnotations(chemblIds):
+    data = runQuery("""
+            select distinct chembl_id,  accession,
+                    mechanism_of_action, tid,component_id, description, 
+                    organism, mesh_id, mesh_heading
+                from chembl_id_lookup
+                        join molecule_dictionary using(chembl_id)
+                        join drug_mechanism using(molregno)
+                        join drug_indication using(molregno)
+                        join target_components using(tid)
+                        join component_sequences using(component_id)
+                where 
+                        chembl_id_lookup.chembl_id in %s
+                order by 1
+            """,(chemblIds,))
+    #return tupleArray2Dict(data)
+    return groupBy(lambda t: t[0], data)
+
 
 def accessionToChembl(accessionIds):
     data = runQuery("""
@@ -41,7 +76,8 @@ def accessionToChembl(accessionIds):
                         accession in %s
                 order by 1
             """,(accessionIds,))
-    return tupleArray2Dict(data)
+    #return tupleArray2Dict(data)
+    return groupBy(lambda t: t[0], data)
 
 def mapToChembl(unknownIds):
     chemblIds = set()
@@ -54,3 +90,7 @@ def mapToChembl(unknownIds):
     #print("final chembl ids: "+str(chemblIds))
 
     return list(chemblIds)
+
+
+#print(chemblTargetAccessions(('CHEMBL26',)))
+#print(chemblTargetAccessionsByAnnotations(('CHEMBL25',)))
