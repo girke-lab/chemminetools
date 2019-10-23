df07ef91e6c7af73a27937a93e9b222799538090
khoran <khoran@cs.ucr.edu>
2019-10-23 11:35:29 -0700

    still working on ei update script

---
+++ chembl_update/update_eiR.R
@@ -0,0 +1,66 @@
+#!/usr/bin/env Rscript
+# requires: ChemmineR,R.utils
+# use: ./eiSearch.R --outfile=output.txt --similarity=0.3 --compounds=10 < input.sdf
+
+library(eiR)
+library(R.utils)
+library(RPostgreSQL)
+
+
+eiConn = dbConnect(dbDriver('PostgreSQL'),dbname='eisearch_chembl_loading',host='chembl.cycqd59qnrsj.us-east-2.rds.amazonaws.com',user='ei_updater',password='kj48nb3n2khlsdfbb')
+chemblConn = dbConnect(dbDriver('PostgreSQL'),dbname='chembl',host='chembl.cycqd59qnrsj.us-east-2.rds.amazonaws.com',user='chembl',password='chembl1889')
+
+buildSDF  = function(outputFile){
+	#fetch list of compound sdfs
+	ids = dbGetQuery(chemblConn,"SELECT  chembl_id FROM compound_structures JOIN molecule_dictionary USING(molregno) ")
+	message("fetched ",nrow(ids)," chembl_ids")
+
+
+	fileConn = file(outputFile,"wt")
+	count=0
+	batchByIndex(ids$chembl_id,function(idBatch){
+
+		query = paste("SELECT  chembl_id, molfile FROM compound_structures JOIN molecule_dictionary USING(molregno) WHERE chembl_id IN ( '",
+					paste(idBatch,collapse="','"),"')",sep="")
+		#message("query: ",query)
+		rows = dbGetQuery(chemblConn,query)
+		count <<- count + nrow(rows)
+		sdfset = c()
+
+		for(i in 1:nrow(rows)){
+			#message("chembl_id: ",rows[i,1])
+			sdfstr = paste(rows[i,1],rows[i,2],"\n$$$$",sep="")
+			sdf = read.SDFset(as(strsplit(sdfstr,"\n"),"SDFstr"),skipErrors=TRUE)
+			if(length(sdfset) == 0)
+				sdfset=sdf
+			else
+				sdfset = c(sdfset,sdf)
+			
+
+			#cat(paste(rows[i,1],rows[i,2],"$$$$\n",sep="\n"),file=fileConn,append=TRUE)
+		}
+		message("current count: ",count)
+		cid(sdfset) = sdfid(sdfset)
+		sdfset = sdfset[which(validSDF(sdfset))];
+		message("done validating")
+		write.SDF(sdfset,file=fileConn)
+		message("done writing")
+	},10000)
+	close(fileConn)
+}
+buildIndex = function(sdfFile){
+#	message("initializing database")
+#	initDb(eiConn)
+#	message("running eiInit from ",sdfFile)
+#	eiInit(sdfFile,conn=eiConn,updateByName=TRUE)
+	message("eiInit done. starting eiMakeDb")
+	runId=eiMakeDb(100,200,conn=eiConn)
+	message("eiMakeDb done")
+	#eiPerformanceTest(runId,conn=eiConn)
+}
+
+
+sdfFile = "chembl.sdf"
+#buildSDF(sdfFile)
+
+#buildIndex(sdfFile)
