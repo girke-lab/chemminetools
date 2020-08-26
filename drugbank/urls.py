a265771e588fb0be19113063acd3380a41504159
khoran <khoran@cs.ucr.edu>
2020-08-26 10:03:55 -0700

    remove usless print messages from drugbank module

--- drugbank/urls.py
+++ drugbank/urls.py
@@ -4,10 +4,8 @@ from django.conf.urls import *
 from drugbank.views import *
 from django.views.decorators.csrf import csrf_exempt
 
-print('\n before url')
 app_name = 'drugbank'
 urlpatterns = [ url(r'^$',
                        csrf_exempt(drugbank_lookup),name = 'drugbank-lookup')]
-print('\n after url')
 
 
